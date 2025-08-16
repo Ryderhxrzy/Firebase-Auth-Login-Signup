55a1453d942d8234eee717ffbae209429b8a85ad
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2025-08-16 14:56:21 +0800

    remove unused script

--- login.php
+++ login.php
@@ -64,30 +64,5 @@
             </div>
         </form>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script type="module">
-        // Import the functions you need from the SDKs you need
-        import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-        import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
-        // TODO: Add SDKs for Firebase products that you want to use
-        // https://firebase.google.com/docs/web/setup#available-libraries
-
-        // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-        const firebaseConfig = {
-            apiKey: "AIzaSyABH0fksrTgT6N3MTdaDSpz6oM5Lm-34mA",
-            authDomain: "login-register-2025.firebaseapp.com",
-            projectId: "login-register-2025",
-            storageBucket: "login-register-2025.firebasestorage.app",
-            messagingSenderId: "283030824710",
-            appId: "1:283030824710:web:fee1c2f50e16d89ef2e087",
-            measurementId: "G-YBPRGS94B4"
-        };
-
-        // Initialize Firebase
-        const app = initializeApp(firebaseConfig);
-        const analytics = getAnalytics(app);
-    </script>
 </body>
 </html>
